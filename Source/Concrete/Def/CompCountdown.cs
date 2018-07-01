ba825362539b83084e31654a401c44743ce142fc
TW <temple_wing@163.com>
2018-07-01 20:12:17 +0800

    change from rot to affect with damp

---
+++ Source/Concrete/Def/CompCountdown.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace Concrete {
+    public class CompCountdown : ThingComp {
+
+        private int ticks = 0;
+        private int maxTick = -1;
+
+        private int getMaxTick() {
+            if(maxTick < 0) {
+                CompPropertiesCountdown pcd = props as CompPropertiesCountdown;
+                maxTick = (int)(pcd.daysCountdown * 60000f - ticks);
+            }
+            return maxTick;
+        }
+
+        private int getRemainingTick() {
+            return getMaxTick() - ticks;
+        }
+
+        public override void CompTick() {
+            ticks++;
+            if(ticks > getMaxTick()) {
+                this.parent.Destroy(DestroyMode.Vanish);
+            }
+        }
+
+        public override string CompInspectStringExtra() {
+            CompPropertiesCountdown pcd = props as CompPropertiesCountdown;
+            return pcd.inspectString + getRemainingTick().ToStringTicksToPeriodVagueMax();
+        }
+    }
+}
---
+++ Source/Concrete/Def/CompPropertiesCountdown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Verse;
+
+namespace Concrete {
+    public class CompPropertiesCountdown : CompProperties {
+        public CompPropertiesCountdown() {
+            compClass = typeof(CompCountdown);
+        }
+        public float daysCountdown = 5;
+        public string inspectString = "";
+    }
+}
