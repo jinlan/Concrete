574685cd7c62651f939f5cb023b5fe7f09748f43
TW <temple_wing@163.com>
2018-07-01 16:17:27 +0800

    big redesign

--- Source/Concrete/Harmony/Thing_PostMake.cs
+++ Source/Concrete/Harmony/Thing_PostMake.cs
@@ -9,7 +9,7 @@ namespace Concrete {
     public class Thing_PostMake {
         private static bool startCheck = false;
         static void Postfix(Thing __instance) {
-            if(__instance != null && __instance.Stuff != null && (__instance.Stuff.defName == "Concrete") && !(__instance is Frame) && startCheckConcrete()) {
+            if(__instance != null && __instance.Stuff != null && (__instance.Stuff.defName == "Concrete" || __instance.Stuff.defName == "Cement") && !(__instance is Frame) && startCheckConcrete()) {
                 ConcreteManager cm = Find.World.GetComponent<ConcreteManager>();
                 if(!cm.contains(__instance)) {
                     __instance.HitPoints = 1;
